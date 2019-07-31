fadb3a4c2217df52b0b018193b55d71c62dd0183
g1zstar <sensiblemagic@gmail.com>
2019-07-30 22:42:33 -0400

    Aran: Fixed Speaker ID 1st Job Advancement Reset stats on 1st Job Advancement because of 5 dex New Beginnings should no longer give 2x Practice Polearm Correct Amount of starting SP for 2nd, 3rd, 4th job advancement 3rd and 4th Job Advancement now correctly gives 5 AP each

--- scripts/quest/q21101s.py
+++ scripts/quest/q21101s.py
@@ -1,13 +1,16 @@
 # 140000000
-LILIN = 1201000
+# LILIN = 1201000
+GIANT_POLEARM = 1201001
 
-sm.setSpeakerID(LILIN)
+# sm.setSpeakerID(LILIN)
+sm.setSpeakerID(GIANT_POLEARM)
 if sm.sendAskYesNo("#b(Are you certain that you were the hero that wielded the #p1201001#? Yes, you're sure. You better grab the #p1201001# really tightly. Surely it will react to you.)#k"):
     sm.giveItem(1142129)
     sm.startQuest(parentID)
     sm.completeQuest(parentID)
     sm.removeSkill(20000297)
     sm.setJob(2100)
+    sm.resetAP(False)
     sm.addSP(5, True)
     # 444
     sm.removeSkill(20001296)
