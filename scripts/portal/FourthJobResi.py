b58049bc0743f726ae5b2f468e93a0461bd65a72
g1zstar <sensiblemagic@gmail.com>
2019-08-13 19:06:43 -0400

    4th Job Resistance portal should no longer set Instance Timer if it doesn't warp you in.

--- scripts/portal/FourthJobResi.py
+++ scripts/portal/FourthJobResi.py
@@ -1,10 +1,18 @@
-if sm.getChr().getJob() == 3211:
+# Portal to the 4th Job Advancement for Main Resistance
+
+validJobIDs = [3211, 3311, 3511, 3711]
+
+charJobID = sm.getChr().getJob()
+if charJobID not in validJobIDs:
+    sm.dispose()
+
+if charJobID == 3211:
     sm.warpInstanceIn(931000300, 0, False) # BATTLE MAGE
-elif sm.getChr().getJob() == 3311:
+elif charJobID == 3311:
     sm.warpInstanceIn(931000301, 0, False) # WILD HUNTER
-elif sm.getChr().getJob() == 3511:
+elif charJobID == 3511:
     sm.warpInstanceIn(931000302, 0, False) # MECHANIC
-elif sm.getChr().getJob() == 3711:
+elif charJobID == 3711:
     sm.warpInstanceIn(931000303, 0, False) # BLASTER
 
 sm.setInstanceTime(15*60)
