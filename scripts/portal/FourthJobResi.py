d611fa429b059130dc053d4c5789e79a735a6a79
g1zstar <sensiblemagic@gmail.com>
2019-08-13 13:24:57 -0400

    Edelstein etc drops (Patrol Robot, Strange Sign, Serpent, Water Thief) Blaster 2nd job advancement drop added Blaster 3rd & 4th job advancement scripted

--- scripts/portal/FourthJobResi.py
+++ scripts/portal/FourthJobResi.py
@@ -1,5 +1,10 @@
-# 310060221
-sm.warp(931000300, 0)
-if sm.hasQuest(23043):
-    sm.completeQuest(23043)
-sm.dispose()
+if sm.getChr().getJob() == 3211:
+    sm.warpInstanceIn(931000300, 0, False) # BATTLE MAGE
+elif sm.getChr().getJob() == 3311:
+    sm.warpInstanceIn(931000301, 0, False) # WILD HUNTER
+elif sm.getChr().getJob() == 3511:
+    sm.warpInstanceIn(931000302, 0, False) # MECHANIC
+elif sm.getChr().getJob() == 3711:
+    sm.warpInstanceIn(931000303, 0, False) # BLASTER
+
+sm.setInstanceTime(15*60)
