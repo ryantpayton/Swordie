d47fbff730382e75b95c10c59de53a19aa4e3c58
g1zstar <sensiblemagic@gmail.com>
2019-08-17 05:34:40 -0400

    Mercedes:     Intro Skip added     Beginner Flash Jump added for the Intro     AP now is properly all in dex     1st job medal now being given     2nd job medal now being given

--- scripts/field/merTutorDrecotion00.py
+++ scripts/field/merTutorDrecotion00.py
@@ -1,2 +1,9 @@
 #Strange Space : Darkness | 4000000
+sm.consumeItem(1352000)
+sm.giveAndEquip(1352000)
+if sm.sendAskYesNo("Would you like to skil the tutorial?"):
+    sm.startQuestNoCheck(24005)
+    sm.warp(101050010)
+    sm.dispose()
+sm.giveSkill(20021181, 1)
 sm.playVideoByScript("Mercedes.avi")
--- scripts/field/merTutorSleep02.py
+++ scripts/field/merTutorSleep02.py
@@ -3,4 +3,7 @@
 if sm.hasQuest(24005): # Cursed Slumber
     sm.completeQuest(24005) # Cursed Slumber
     sm.jobAdvance(2300) # Mercedes
-    sm.giveSkill(20021166, 0) # Remove the Beginner Stunning Strike Skill
+    sm.resetAP(False, 2300)
+    sm.giveItem(1142336)
+    sm.removeSkill(20021166) # Remove the Beginner Stunning Strike Skill
+    sm.removeSkill(20021181) # Remove the Beginner Flash Jump Skill
