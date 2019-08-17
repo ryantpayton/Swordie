f37b7d82c4ac7b1b958e09d64d6618bacaa6f14e
g1zstar <sensiblemagic@gmail.com>
2019-08-17 02:13:05 -0400

    Cygnus Knights 1st job wrong AP values and completing quest too late, now resets AP upon job advance and completes quest before chatting.

--- scripts/quest/q20861e.py
+++ scripts/quest/q20861e.py
@@ -1,13 +1,9 @@
 # The Path of a Dawn Warrior - Completion
-from net.swordie.ms.enums import Stat
+sm.setSpeakerID(1101003) # Mihile
 
 sm.jobAdvance(1100) # Dawn Warrior 1st Job
-curLevel = sm.getChr().getLevel()
-if sm.getChr().getStat(Stat.str) >= 50:
-    sm.getChr().addStatAndSendPacket(Stat.str, -(curLevel * 5)) # deduct automatic AP given to str while leveling
-    sm.getChr().addStatAndSendPacket(Stat.ap, (curLevel * 5)) # and add it back to AP
-sm.setSpeakerID(1101003) # Mihile
-sm.sendSayOkay("Congratulations, you are now a thunder breaker! I have added 4 SP, enjoy your journey!")
-sm.completeQuest(parentID)
+sm.resetAP(False)
 sm.giveItem(1402001) # Wooden Sword (2H)
-sm.dispose()
+
+sm.completeQuest(parentID)
+sm.sendSayOkay("Congratulations, you are now a thunder breaker! I have added 4 SP, enjoy your journey!")
--- scripts/quest/q20862e.py
+++ scripts/quest/q20862e.py
@@ -1,13 +1,9 @@
 # The Path of a Blaze Wizard - Completion
-from net.swordie.ms.enums import Stat
+sm.setSpeakerID(1101004) # Oz
 
 sm.jobAdvance(1200) # Blaze Wizard 1st Job
-curLevel = sm.getChr().getLevel()
-if sm.getChr().getStat(Stat.str) >= 50:
-    sm.getChr().addStatAndSendPacket(Stat.str, -(curLevel * 5)) # deduct automatic AP given to str while leveling
-    sm.getChr().addStatAndSendPacket(Stat.ap, (curLevel * 5)) # and add it back to AP
-sm.setSpeakerID(1101004) # Oz
-sm.sendSayOkay("Congratulations, you are now a blaze wizard! I have added 5 AP and 5 SP, enjoy your journey!")
-sm.completeQuest(parentID)
+sm.resetAP(False)
 sm.giveItem(1382000) # Wooden Staff
-sm.dispose()
+
+sm.completeQuest(parentID)
+sm.sendSayOkay("Congratulations, you are now a blaze wizard! I have added 5 AP and 5 SP, enjoy your journey!")
