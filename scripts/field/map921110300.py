375091332e2d4039ae70d5c279aa3ae4781410cf
g1zstar <sensiblemagic@gmail.com>
2019-08-21 18:39:15 -0400

    Shade Third & Fourth Job Advancement Scripted

---
+++ scripts/field/map921110300.py
@@ -0,0 +1,6 @@
+if sm.hasQuest(38074):
+    sm.removeEscapeButton()
+    sm.setPlayerAsSpeaker()
+    sm.flipSpeaker()
+    sm.sendNext("This castle used to be so grand, and now it's all in ruins. If I remember correctly there's a way in there somewhere.")
+    sm.completeQuestNoRewards(SHADE_THIRD_JOB)
