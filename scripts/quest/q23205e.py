81cb87d654f201cb766d03deb8ef46a3cf115535
g1zstar <sensiblemagic@gmail.com>
2019-08-21 20:12:38 -0400

    Demon Beginner Intro Skills should no longer be available after Intro

--- scripts/quest/q23205e.py
+++ scripts/quest/q23205e.py
@@ -4,7 +4,7 @@ ARKARIUM = 2159309
 sm.completeQuestNoRewards(parentID)
 sm.deleteQuest(parentID)
 for i in range(5):
-    if not sm.hasSkill(SKILLS[i]):
-        sm.giveSkill(SKILLS[i], 0)# remove the skill
+    if sm.hasSkill(SKILLS[i]):
+        sm.removeSkill(SKILLS[i])# remove the skill
 sm.removeNpc(ARKARIUM)
 sm.warpInstanceIn(927000070, 0)
