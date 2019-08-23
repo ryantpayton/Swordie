19a1aa2b4827eec92c5ec1e5783c983e047fc7fe
g1zstar <sensiblemagic@gmail.com>
2019-08-23 01:12:43 -0400

    Resistance    2nd job Teleport Fixed for WH & Mech    Fourth Job Teleport now requires key card from job advance quests    Fourth Job second Teleport fixed for WH & Mech    Medals moved from job advancement quests to Medal Auto Start Quests

--- scripts/portal/FourthJobResi.py
+++ scripts/portal/FourthJobResi.py
@@ -1,5 +1,11 @@
 # Portal to the 4th Job Advancement for Main Resistance
 
+GELIMERS_KEY_CARD = 4032743
+
+if not sm.hasItem(GELIMERS_KEY_CARD):
+    sm.chat("The entrance is locked. It can only be unlocked by Gelimer.")
+    sm.dispose()
+
 validJobIDs = {
     3211 : 931000300,
     3311 : 931000301,
--- scripts/quest/q29944s.py
+++ scripts/quest/q29944s.py
@@ -4,5 +4,6 @@ medal = 1142245
 
 if sm.canHold(medal):
     sm.chatScript("You have earned a new medal.")
+    sm.giveItem(medal)
     sm.startQuest(parentID)
     sm.completeQuest(parentID)
