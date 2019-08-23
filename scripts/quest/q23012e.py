49e460cd39038f4feb38af5d674c3709a00b9c1a
g1zstar <sensiblemagic@gmail.com>
2019-08-23 00:42:23 -0400

    BaM and WH 2nd Job Drops Added WH    1st job advance fixed    Inventory Space for 1st job Checked    AP is reset into dex now    2nd - 4th job advancement added

--- scripts/quest/q23012e.py
+++ scripts/quest/q23012e.py
@@ -1,17 +1,20 @@
 # 23012 - Wild Hunter 1st job advancement quest
-from net.swordie.ms.enums import Stat
 
 XBOW_ID = 1462092
 
-sm.setSpeakerID(2152003) # Belle
-if chr.getJob() == 3000 and chr.getLevel() >= 10:
-    sm.jobAdvance(3300)
+sm.setSpeakerID(2151002) # Belle
+if sm.sendAskYesNo("Would you like to become a Wild Hunter?"):
+    if not sm.canHold(XBOW_ID) or not sm.canHold(2061000):
+        sm.sendSayOkay("Please make some space in your Equipment or Use Inventory.")
+        sm.dispose()
     sm.completeQuest(23012)
+    sm.jobAdvance(3300)
+    sm.resetAP(False, 3300)
     sm.giveItem(XBOW_ID, 1)
     sm.giveSkill(30001061) # Capture
     sm.giveSkill(30001062) # Call of the Hunter
     sm.giveItem(2061000, 2000)
-    sm.sendSayOkay("Congratulations, you are now a battle mage! I have given you some SP and items to start out with, enjoy!")
+    sm.sendSayOkay("Congratulations, you are now a Wild Hunter! I have given you some SP and items to start out with, enjoy!")
 else:
-    sm.sendSayOkay("I don't think you are quite ready to become a Wild Hunter.")
+    sm.sendSayOkay("Come back when you're ready.")
 sm.dispose()
