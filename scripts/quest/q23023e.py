59daa5bc50f9e0038ddcf354f4e428519e2cf314
g1zstar <sensiblemagic@gmail.com>
2019-08-23 00:38:34 -0400

    Battle Mage    2nd Job Drop    Make sure we have space for 1st job advance    4th job quest sequence corrected

--- scripts/quest/q23023e.py
+++ scripts/quest/q23023e.py
@@ -1,11 +1,10 @@
 # 23023 - 2nd job advancement Battle Mage
-from net.swordie.ms.enums import Stat
 
 sm.setSpeakerID(2151001)
-if sm.hasItem(4032737, 1) and chr.getJob() == 3200:
+if sm.sendAskYesNo("Are you ready to advance to the next level?"):
+    sm.completeQuest(parentID)
     sm.jobAdvance(3210)
-    sm.completeQuest(23023)
+    sm.consumeItem(4032737)
     sm.sendSayOkay("Good job on finding the report. I've molded you into the next level of being a Battle Mage.")
 else:
-    sm.sendSayOkay("I still think you're missing the report.")
-sm.dispose()
+    sm.sendSayOkay("Come back when you're ready.")
